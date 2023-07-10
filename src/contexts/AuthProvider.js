afb70ef7eb342d9ab9f54c136cd7ac3e8d951cdc
Amran Hossain <amran.h.akash@gmail.com>
2023-07-10 23:50:38 +0600

    private route added

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -8,15 +8,21 @@ const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -28,6 +34,7 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log("user Observing");
             setUser(currentUser)
+            setLoading(false);
         });
 
         return () => unsubscribe();
@@ -38,7 +45,8 @@ const AuthProvider = ({ children }) => {
         signIn,
         logOut,
         updateUser,
-        user
+        user,
+        loading
     }
 
     return (
